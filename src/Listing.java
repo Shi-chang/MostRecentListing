32f902910c82d92578b0340c147495dddcd158b5
Shi-chang <shichangye@outlook.com>
2023-02-16 22:20:49 -0800

    Delete unused methods

--- src/Listing.java
+++ src/Listing.java
@@ -39,15 +39,6 @@ public class Listing {
     }
 
     /**
-     * Sets the id of the listing.
-     *
-     * @param id the new id for the listing
-     */
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    /**
      * Gets the address of the listing.
      *
      * @return the address of the listing.
@@ -57,57 +48,11 @@ public class Listing {
     }
 
     /**
-     * Sets the address for the listing.
-     *
-     * @param address the new address for the listing
-     */
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    /**
      * Gets the year of the listing.
      *
      * @return the year of the listing
      */
     public int getYear() {
         return year;
-    }
-
-    /**
-     * Sets the year of the listing.
-     *
-     * @param year the new year for the listing.
-     */
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    /**
-     * Compares if the other object is equal to this listing.
-     *
-     * @param o the other object to be compared with this listing
-     * @return true if they are equal, false otherwise
-     */
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Listing listing = (Listing) o;
-        return address.equals(listing.address);
-    }
-
-    /**
-     * Returns the hashcode of the listing.
-     *
-     * @return the hashcode of the listing
-     */
-    @Override
-    public int hashCode() {
-        return Objects.hash(address);
     }
 }
